142284af70954cacb084a5b0859516892ef5ea47
indraahmadiskandar <indraiskandar10@gmail.com>
2024-01-15 19:35:59 +0700

    -

--- assets/js/404.js
+++ assets/js/404.js
@@ -20,7 +20,7 @@ document.onkeydown = function(e) {
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
      return false;
   }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//      return false;
+//   }
 }
